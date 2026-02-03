dce548f05223e11fe032a58eb20eccae65fa8710
Riccardo De Icco <riccardo.de.icco@gmail.com>
2026-02-03 23:28:27 +0100

    Fix origin

--- solution/backend/main_server/socket.js
+++ solution/backend/main_server/socket.js
@@ -19,7 +19,7 @@ function initSocket(server) {
      */
     const io = new Server(server, {
         cors: {
-            origin: "http://localhost:63344",
+            origin: ["http://localhost:63342", "http://localhost:63344", "http://127.0.0.1:5500"],
             methods: ["GET", "POST"]
         }
     });

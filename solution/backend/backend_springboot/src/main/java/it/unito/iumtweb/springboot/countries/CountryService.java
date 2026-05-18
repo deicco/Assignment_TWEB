00c9188ccb42980e9882ab444a3b1db46e4358a3
Riccardo De Icco <riccardo.de.icco@gmail.com>
2026-05-18 13:42:29 +0200

    Add constructor methods

--- solution/backend/backend_springboot/src/main/java/it/unito/iumtweb/springboot/countries/CountryService.java
+++ solution/backend/backend_springboot/src/main/java/it/unito/iumtweb/springboot/countries/CountryService.java
@@ -20,7 +20,6 @@ public class CountryService {
     public CountryService(CountryRepository countryRepository) {
         this.countryRepository = countryRepository;
     }
-
     /**
      * Retrieves all country entries with pagination.
      */
